c0840b035c43552d7c169736ae37b0bbc765b236
josephsteenhuisen <110561019+josephsteenhuisen@users.noreply.github.com>
2023-04-28 21:06:54 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "week11.h"
+
+int main(){
+    Fraction a = create_fraction(1, 2);
+    Fraction b = create_fraction(3, 4);
+    Fraction c = create_fraction(5, 6);
+    Fraction frac2 = multiply_fraction_pair(a, b);
+    Fraction frac2 = multiply_fraction_pair(a, c);
+    std::cout << frac1.fraction.first << std::endl;
+    std::cout << frac1.fraction.second << std::endl;
+     std::cout << frac2.fraction.first << std::endl;
+    std::cout << frac2.fraction.second << std::endl;
+   
+}
